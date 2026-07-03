825f8a17d556bb6be527f1d42b760f825bb98adb
rajes564 <122001941+rajes564@users.noreply.github.com>
2026-07-03 14:44:23 +0530

    Update API base URL to new endpoint

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://ttwreis-backend.onrender.com/api',
+  baseURL: 'https://ttwreis-backend-2wzl.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
   timeout: 15000,
 })
